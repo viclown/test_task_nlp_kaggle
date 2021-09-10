7dd6430b4b011f5a07b35e54a44c8c0e09f8dc5b
viclown <76227700+viclown@users.noreply.github.com>
2021-09-10 19:35:41 +0300

    fixed functions, better score

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ import numpy as np
 import csv
 from preprocess_functions import tfidf
 
-np.random.seed(0)
-
 train = pd.read_csv("train.csv")
 test = pd.read_csv("test.csv")
 
@@ -22,24 +20,24 @@ x_test = [test_n[i] + " " + test_d[i] for i in range(len(test_n))]
 
 vect_traintest = x_train + x_test
 
-tfidf(vect_traintest)
+vectorized_corpus = tfidf(vect_traintest)
 
-x_train_vec = vect_traintest[:408]
-x_test_vec = vect_traintest[408:]
+x_train_vec = vectorized_corpus[:408]
+x_test_vec = vectorized_corpus[408:]
 
-categories = {'Менеджер': 0,
-    'Искусство': 1,
-    'Рабочий': 2,
-    'Дизайнер': 3,
-    'Специалист': 4,
-    'СМИ': 5,
-    'Врач': 6,
-    'other': 7,
-    'Инженер': 8,
-    'IT': 9,
-    'Право': 10,
-    'Учитель': 11,
-    'Агент': 12}
+categories = {'Агент': 0,
+ 'Учитель': 1,
+ 'Дизайнер': 2,
+ 'IT': 3,
+ 'Инженер': 4,
+ 'Специалист': 5,
+ 'other': 6,
+ 'Рабочий': 7,
+ 'СМИ': 8,
+ 'Менеджер': 9,
+ 'Право': 10,
+ 'Искусство': 11,
+ 'Врач': 12}
 
 y_train = list(train["category"])
 for i in range(len(y_train)):
--- preprocess_functions.py
+++ preprocess_functions.py
@@ -2,58 +2,67 @@ from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
 import math
 
-def del_punct(corpus):
-    symbols = [".", ",", "/", "?", ">", "<", "!", "", ":", "-", "—", "«", "»"]
+stemmer = SnowballStemmer(language = "russian")
+stop_words = stopwords.words("russian")
+
+def get_punct(corpus: list) -> list:
+    corpus_symbols = set(",".join(corpus))
+    symbols = [i for i in corpus_symbols if i.isalnum() != 1 and i != " "]
+    return symbols
+
+def del_punct(corpus: list) -> list:
+    symbols = get_punct(corpus)
     for i in range(len(corpus)):
         doc = [s.lower() if s not in symbols else " " for s in corpus[i]]
         corpus[i] = "".join(doc)
     return corpus
 
-def stem_corpus(corpus):
-    stemmer = SnowballStemmer(language = "russian")
+def stem_corpus(corpus: list) -> list:
     for i in range(len(corpus)):
         doc = [stemmer.stem(w) for w in corpus[i].split()]
         corpus[i] = " ".join(doc)
     return corpus
 
-def del_stop(corpus):
-    stop_words = stopwords.words("russian")
+def del_stop(corpus: list) -> list:
     for i in range(len(corpus)):
         doc = [w for w in corpus[i].split() if w not in stop_words]
         corpus[i] = " ".join(doc)
     return corpus
 
-def tfidf(corpus):
-    corpus = del_punct(corpus)
-    corpus = del_stop(corpus)
-    corpus = stem_corpus(corpus)
+def tfidf(corpus: list) -> list:
+    vect_corpus = corpus.copy()
+    vect_corpus = del_punct(vect_corpus)
+    vect_corpus = del_stop(vect_corpus)
+    vect_corpus = stem_corpus(vect_corpus)
     
     num_words = {}
     cnt = 0
     
-    for doc in corpus:
+    for doc in vect_corpus:
         for word in doc.split():
             if word in num_words:
                 num_words[word][cnt] += 1
             else:
-                num_words.update({word: [0]*len(corpus)})
+                num_words.update({word: [0]*len(vect_corpus)})
                 num_words[word][cnt] += 1
         cnt += 1
         
     tf = {}
     for word in num_words.keys():
-        tf.update({word: [0]*len(corpus)})
-        for i in range(len(corpus)):
-            tf[word][i] = num_words[word][i]/len(corpus[i])
+        tf.update({word: [0]*len(vect_corpus)})
+        for i in range(len(vect_corpus)):
+            tf[word][i] = num_words[word][i]/len(vect_corpus[i])
             
     idf = {}
     for word in num_words.keys():
-        idf_w = math.log(len(corpus) 
+        idf_w = math.log(len(vect_corpus) 
                 / (len(num_words[word]) - num_words[word].count(0)))
         idf.update({word: idf_w})
         
-    for i in range(len(corpus)):
+    for i in range(len(vect_corpus)):
         new_doc = []
         for word in num_words.keys():
             new_doc.append(tf[word][i]*idf[word])
-        corpus[i] = new_doc
+        vect_corpus[i] = new_doc
+        
+    return vect_corpus
